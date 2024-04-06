382631c5b0de64b3aa4b270298dd86a3f9a9ba5a
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-04-06 12:56:20 +0200

    Creation de l'objet de recherche

--- carrerarecheche.cpp
+++ carrerarecheche.cpp
@@ -1,3 +1,87 @@
 #include "carrerarecheche.h"
 
-CArreraRecheche::CArreraRecheche() {}
+CArreraRecheche::CArreraRecheche()
+{
+
+}
+
+CArreraRecheche::~CArreraRecheche()
+{
+
+}
+
+void CArreraRecheche::searchDuckduckgo(QString q)
+{
+    QString url = "https://duckduckgo.com/?q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchGoogle(QString q)
+{
+    QString url = "https://www.google.com/search?q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchQwant(QString q)
+{
+    QString url = "https://www.qwant.com/?l=fr&q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchEcosia(QString q)
+{
+    QString url = "https://www.ecosia.org/search?method=index&q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchBrave(QString q)
+{
+    QString url = "https://search.brave.com/search?q="+q+"&source=web";
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchBing(QString q)
+{
+    QString url = "https://www.bing.com/search?q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchAmazon(QString q)
+{
+    QString url = "https://www.amazon.fr/s?k="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchWikipedia(QString q)
+{
+    QString url = "https://fr.wikipedia.org/wiki/"+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchReverso(QString q)
+{
+    QString url = "https://www.reverso.net/traduction-texte#sl=fra&tl=eng&text="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchWordreference(QString q)
+{
+    QString url = "https://www.wordreference.com/fren/"+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchYTmusic(QString q)
+{
+    QString url = "https://music.youtube.com/search&q="+q;
+    QDesktopServices::openUrl(QUrl(url));
+}
+
+void CArreraRecheche::searchAll(QString q)
+{
+    searchGoogle(q);
+    searchBing(q);
+    searchBrave(q);
+    searchQwant(q);
+    searchDuckduckgo(q);
+    searchEcosia(q);
+}
--- carrerarecheche.h
+++ carrerarecheche.h
@@ -1,11 +1,31 @@
 #ifndef CARRERARECHECHE_H
 #define CARRERARECHECHE_H
 
+#include <QDesktopServices>
+#include <QUrl>
+#include <string.h>
+
+using namespace std ;
+
 class CArreraRecheche
 {
+private :
+    bool etatInternet ;
 public:
     CArreraRecheche();
-
+    ~CArreraRecheche();
+    void searchDuckduckgo(QString q);
+    void searchGoogle(QString q);
+    void searchQwant(QString q);
+    void searchEcosia(QString q);
+    void searchBrave(QString q);
+    void searchBing(QString q);
+    void searchAmazon(QString q);
+    void searchWikipedia(QString q);
+    void searchReverso(QString q);
+    void searchWordreference(QString q);
+    void searchYTmusic(QString q);
+    void searchAll(QString q);
 };
 
 #endif // CARRERARECHECHE_H
